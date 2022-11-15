feb043abcbbaa09569a9d25680aa2af97cdd33f7
Emil <emilons@hotmail.com>
2022-11-15 10:22:49 +0100

    #2 working db integration, added seeding data

--- Booking/DataAccess/OfficeDbContext.cs
+++ Booking/DataAccess/OfficeDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using Booking.Models.Domain;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace Booking.DataAccess
@@ -21,6 +24,61 @@ namespace Booking.DataAccess
                 .HasOne<Office>(user => user.Office)
                 .WithMany(office => office.Users)
                 .HasForeignKey(user => user.OfficeId);
+
+            // Seed dummy data
+            AddSeedingData(modelBuilder);
+        }
+
+        private static void AddSeedingData(ModelBuilder modelBuilder)
+        {
+            // Add Office
+            modelBuilder.Entity<Office>().HasData(new Office
+            {
+                Id = 1,
+                Name = "Lille Grensen",
+                Capacity = 20
+            });
+
+            // Add Users
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = 1,
+                Name = "Ted Mosby",
+                IsSignedIn = false,
+                OfficeId = 1
+            });
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = 2,
+                Name = "Marshall Eriksen",
+                IsSignedIn = true,
+                OfficeId = 1
+            });
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = 3,
+                Name = "Lily Aldrin",
+                IsSignedIn = true,
+                OfficeId = 1
+            });
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = 4,
+                Name = "Barney Stinson",
+                IsSignedIn = false,
+                OfficeId = 1
+            });
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = 5,
+                Name = "Robin Scherbatsky",
+                IsSignedIn = false,
+                OfficeId = 1
+            });
         }
     }
 }
--- Booking/Program.cs
+++ Booking/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<OfficeDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConntection")));
+builder.Services.AddDbContext<OfficeDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection")));
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
 
