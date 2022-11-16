32b34b3f61b693a45f7875d4374042caae4b2b29
Mads-Power <mr.kolkin@gmail.com>
2022-11-16 12:42:23 +0100

    prettier settings, format

--- Booking/client/src/App.tsx
+++ Booking/client/src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
-import Button from './components/Button';
-import BookingForm from './components/BookingForm';
-import CheckIn from './components/checkin';
+import { useState } from "react";
+import Button from "./components/Button";
+import BookingForm from "./components/BookingForm";
+import CheckIn from "./components/CheckIn";
 
 function App() {
   return (
-    <div className='App'>
-      <div>
-        4/15
-      </div>
-      <CheckIn/>
+    <div className="App">
+      <div>4/15</div>
+      <CheckIn />
       <BookingForm />
-
     </div>
   );
 }
