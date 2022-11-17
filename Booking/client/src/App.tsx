444cf0e8784ce19065068cb1bafd02d5f2e3633f
Nnaemeka Echem <ne@itverket.no>
2022-11-17 10:33:15 +0100

    package update

--- Booking/client/src/App.tsx
+++ Booking/client/src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import Button from './components/Button';
 import BookingForm from './components/BookingForm';
+import CheckIn from './components/checkin';
 
 function App() {
   return (
     <div className='App'>
+      <div>
+        4/15
+      </div>
+      <CheckIn/>
       <BookingForm />
+
     </div>
   );
 }
