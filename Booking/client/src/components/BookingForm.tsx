df4401772db33fe88820e11229d7c964b04073f0
Nnaemeka Echem <ne@itverket.no>
2022-11-17 10:31:19 +0100

    package update

---
+++ Booking/client/src/components/BookingForm.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import Button from './button';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import listOfPeople from './../data/list-of-people.json';
+
+const BookingForm = () => {
+  const [members, setMembers] = useState(listOfPeople.developers);
+  return (
+    <form action=''>
+      <input placeholder='Søk...' type='text' />
+      {members.map(member => (
+        <Button name={member.name} key={member.id} />
+      ))}
+    </form>
+  );
+};
+
+export default BookingForm;
