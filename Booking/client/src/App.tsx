60786e2b650aa0cd164eac8c201e2fc6695a15c8
Mads-Power <mr.kolkin@gmail.com>
2022-11-18 07:51:33 +0100

    react router + home page

--- Booking/client/src/App.tsx
+++ Booking/client/src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import Button from "./components/Button";
 import BookingForm from "./components/BookingForm";
-import CheckIn from "./components/CheckIn";
+import CheckIn from "./components/Home";
 import List from "./components/List";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <List />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
     </div>
   );
 }
---
+++ Booking/client/src/components/Home.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const Home = () => {
+  return <div>hjem </div>;
+};
+
+export default Home;
