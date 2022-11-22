8c81b06cfddbfcabb927891266658ba45efb0d20
Emil <emilons@hotmail.com>
2022-11-22 09:45:05 +0100

    #6 #11 add CRUD for Office

--- Booking/Controllers/OfficeController.cs
+++ Booking/Controllers/OfficeController.cs
@@ -19,15 +19,93 @@ namespace Booking.Controllers
         private readonly IMapper _mapper;
         private readonly OfficeService _officeService;
         private readonly UserService _userService;
+        private readonly RoomService _roomService;
+        private readonly SeatService _seatService;
 
-        public OfficeController(IMapper mapper, OfficeService officeService, UserService userService)
+        public OfficeController(IMapper mapper, OfficeService officeService, UserService userService, RoomService roomService, SeatService seatService)
         {
             _mapper = mapper;
             _officeService = officeService;
             _userService = userService;
+            _roomService = roomService;
+            _seatService = seatService;
         }
 
         // HTTP requests
+
+        // get: all seats
+        [HttpGet]
+        public async Task<ActionResult<List<OfficeReadDTO>>> GetAllOffices()
+        {
+            var offices = await _officeService.GetAllOffices();
+
+            return _mapper.Map<List<OfficeReadDTO>>(offices);
+        }
+
+        // get: office info
+        [HttpGet("{officeId}")]
+        public async Task<ActionResult<OfficeReadDTO>> GetOffice(int officeId)
+        {
+            try
+            {
+                var office = await _officeService.GetOfficeAsync(officeId);
+
+                return _mapper.Map<OfficeReadDTO>(office);
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+        }
+
+        // post: add new office
+        [HttpPost]
+        public async Task<ActionResult<OfficeCreateDTO>> PostOffice(OfficeCreateDTO dtoOffice)
+        {
+            // validate request and if not validated return BadRequest()?
+
+            var domainOffice = _mapper.Map<Office>(dtoOffice);
+
+            await _officeService.AddAsync(domainOffice);
+
+            return CreatedAtAction("GetOffice",
+                new { officeId = domainOffice.Id },
+                _mapper.Map<OfficeReadDTO>(domainOffice));
+        }
+
+        // put: ...
+        [HttpPut("officeId")]
+        public async Task<IActionResult> PutOffice(int officeId, OfficeEditDTO officeDto)
+        {
+            if (officeId != officeDto.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!_officeService.OfficeExists(officeId))
+            {
+                return NotFound();
+            }
+
+            var domainOffice = _mapper.Map<Office>(officeDto);
+            await _officeService.UpdateAsync(domainOffice);
+
+            return NoContent();
+        }
+
+        // delete: delete office
+        [HttpDelete("officeId")]
+        public async Task<IActionResult> DeleteOffice(int officeId)
+        {
+            if (!_officeService.OfficeExists(officeId))
+            {
+                return NotFound();
+            }
+
+            await _officeService.DeleteAsync(officeId);
+
+            return NoContent();
+        }
     }
 }
 
--- Booking/Services/OfficeService.cs
+++ Booking/Services/OfficeService.cs
@@ -16,6 +16,44 @@ namespace Booking.Services
 		{
 			_context = context;
 		}
+
+        public bool OfficeExists(int officeId)
+        {
+            return _context.Offices.Find(officeId) != null;
+        }
+
+        public async Task<List<Office>> GetAllOffices()
+        {
+            return await _context.Offices.ToListAsync();
+        }
+
+        public async Task<Office?> GetOfficeAsync(int officeId)
+        {
+            return await _context.Offices.FindAsync(officeId);
+        }
+
+        public async Task<Office> AddAsync(Office newOffice)
+        {
+            _context.Offices.Add(newOffice);
+            await _context.SaveChangesAsync();
+            return newOffice;
+        }
+
+        public async Task UpdateAsync(Office updatedOffice)
+        {
+            _context.Entry(updatedOffice).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int officeId)
+        {
+            var office = await _context.Offices.FindAsync(officeId);
+            if (office != null)
+            {
+                _context.Offices.Remove(office);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
 
