b62759603ba01d1914b0b0a43da600a37536f8ed
Mads-Power <mr.kolkin@gmail.com>
2022-11-22 12:36:52 +0100

    adjustments

--- Booking/client/src/components/NavBar.tsx
+++ Booking/client/src/components/NavBar.tsx
@@ -1,10 +1,8 @@
 import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-const MarginRight = styled.div`
-  margin-right: 1rem;
-`;
 export function Navbar() {
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
@@ -13,9 +11,8 @@ export function Navbar() {
           <Nav.Link to="/" as={NavLink}>
             Home
           </Nav.Link>
-
           <Nav.Link to="/OfficeLayout" as={NavLink}>
-            Book plass
+            Booke plass
           </Nav.Link>
         </Nav>
       </Container>
