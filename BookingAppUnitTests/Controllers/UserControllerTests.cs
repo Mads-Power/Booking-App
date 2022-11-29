c2f8746c8cbbf190afa2458bd7b940a90a5de1fa
Emil <emilons@hotmail.com>
2022-11-29 14:38:29 +0100

    #14 init unit tests project

---
+++ BookingAppUnitTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AutoMapper;
+using BookingApp.Controllers;
+using BookingApp.Services;
+using Moq;
+
+namespace BookingAppUnitTests.Controllers
+{
+	public class UserControllerTests
+	{
+  //      private readonly Mock<UserService> _mockUserService;
+  //      private readonly Mock<BookingService> _mockBookingService;
+  //      private readonly Mock<SeatService> _mockSeatService;
+  //      private readonly UserController _controller;
+  //      private readonly IMapper _mapper;
+
+  //      public UserControllerTests()
+		//{
+  //          _mockUserService = new Mock<UserService>();
+  //          _mockBookingService = new Mock<BookingService>();
+  //          _mockSeatService = new Mock<SeatService>();
+  //          _controller = new UserController(_mapper,_mockUserService, _mockSeatService,_mockBookingService);
+  //      }
+	}
+}
+
