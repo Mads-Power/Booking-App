7c3e447bbd8fb615f8d7be2af665eb41943e3466
Mads-Power <mr.kolkin@gmail.com>
2022-12-09 12:34:37 +0100

    no text decoration, underline

--- Booking/client/src/components/Layout/OfficeLayout.tsx
+++ Booking/client/src/components/Layout/OfficeLayout.tsx
@@ -7,14 +7,14 @@ const OfficeLayout = () => {
   return (
     <>
       <Container>
-        <Link to="/mainLayout">
+        <Link to="/mainLayout" style={{ textDecoration: "none" }}>
           <Stack direction="row" spacing={2}>
             <Button size="large">OSLO</Button>
             {/* <Button disabled>Disabled</Button>
           <Button href="#text-buttons">Link</Button> */}
           </Stack>
         </Link>
-        <Button disabled size="large">
+        <Button disabled size="large" style={{ textDecoration: "none" }}>
           DRAMMEN
         </Button>
       </Container>
