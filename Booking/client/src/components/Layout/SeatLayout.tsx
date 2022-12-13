1418b49dd94a55cbadaf560092bc54ce60f21791
Nnaemeka Echem <ne@itverket.no>
2022-12-13 14:44:50 +0100

    modified SeatLayout and putBookingBook

--- Booking/client/src/components/Layout/SeatLayout.tsx
+++ Booking/client/src/components/Layout/SeatLayout.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { useSeat } from "../../features/seats/api/getSeat";
-import { useBook } from "../../features/bookings/api/putBookingBook";
-import { CircularProgress, Button, Container, colors } from "@mui/material";
-import { useParams } from "react-router-dom";
-import Chari from "../../features/seats/components/chair.png" ;
+import React from 'react';
+import { useSeat } from '../../features/seats/api/getSeat';
+import { CreateBooking, useBook } from '../../features/bookings/api/putBookingBook';
+import { CircularProgress, Button, Container, colors } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import Chari from '../../features/seats/components/chair.png';
+import { Booking } from '../../features/bookings/types';
 
 const SeatLayout = () => {
   let { seatId } = useParams();
@@ -16,18 +17,26 @@ const SeatLayout = () => {
   if (error) {
     return <h4>No Rooms Found</h4>;
   }
-  const handleBook = () => (
-    useBook(data?.id, 1, "2022-12-24T12:00:00.000+01").mutate();
-  );
+  const bookingData = {
+    data: {
+      seatId: data?.id,
+      userId: 1,
+      date: '2022-12-24T12:00:00.000+01',
+    },
+  } as CreateBooking;
+
+  const mutation = useBook();
+  const handleBook = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    mutation.mutate(bookingData);
+  };
 
   return (
     <>
-      <img src={Chari}/>
+      <img src={Chari} />
       <h1>Seat name: {data?.name}</h1>
       <div>Room number {data?.roomId}</div>
-      <Button onClick={handleBook}>
-        Book seat
-      </Button>
+      <Button onClick={handleBook}>Book seat</Button>
       {data!.bookings.map(b => (
         <div key={b.id}>Date: {b.date}</div>
       ))}
