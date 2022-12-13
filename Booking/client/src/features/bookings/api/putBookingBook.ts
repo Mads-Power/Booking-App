92d06d83f2c705c0b74b3f0acb53db3fec75e1e2
Nnaemeka Echem <ne@itverket.no>
2022-12-13 14:44:50 +0100

    modified SeatLayout and putBookingBook

--- Booking/client/src/features/bookings/api/putBookingBook.ts
+++ Booking/client/src/features/bookings/api/putBookingBook.ts
@@ -1,24 +1,28 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
-import { Booking } from "../types";
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { Booking } from '../types';
 
-const url = "http://localhost:51249/api/Booking/Book";
-export const putBookingBook = async (seatId: number, userId: number, date: string) => {
-  const body = new URLSearchParams();
-  body.append("seatId",seatId.toString());
-  body.append("userId",userId.toString());
-  body.append("date",date);
-  console.log(body);
-  const requestOptions = {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: body,
+export type CreateBooking = {
+  data: {
+    seatId: number;
+    userId: number;
+    date: string;
   };
-  const res = await fetch(url, requestOptions);
-  return res.json();
+};
+
+const url = 'http://localhost:51249/api/Booking/Book';
+export const putBookingBook = async ({ data }: CreateBooking) => {
+  const requestOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      data,
+    }),
+  };
+  const res = fetch(url, requestOptions);
 };
 
 export const useBook = () => {
-  return useMutation<Booking>({
-    mutationFn: (seatId: number, userId: number, date: string) => putBookingBook(seatId, userId, date)
+  return useMutation({
+    mutationFn: putBookingBook,
   });
 };
--- Booking/client/src/features/seats/components/GetSeat.tsx
+++ Booking/client/src/features/seats/components/GetSeat.tsx
@@ -4,20 +4,9 @@ import Dialog from '@mui/material/Dialog';
 import { DialogActions, DialogContent } from '@mui/material';
 import Button from '@mui/material/Button';
 
-const GetSeat = ({ seatId }: Seat) => {
+const GetSeat = () => {
   return (
     <>
-      {/* <Dialog open={open} onClose={handleClose}>
-        <DialogContent>
-          <img src='./chair.png' alt='' />
-          <p>Rom: ...</p>
-          <p>Setenummer: {seatId}</p>
-          <p>Navn: </p>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Book Seat</Button>
-        </DialogActions>
-      </Dialog> */}
       <p>hehelwofrhle</p>
     </>
   );
