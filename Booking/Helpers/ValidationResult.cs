ce31c419c2d3062b16ee6b0069c1efdbabd9dcf6
Emil <emilons@hotmail.com>
2023-01-06 14:58:34 +0100

    update backend datevalidation regex to demand midnight time

--- Booking/Helpers/ValidationResult.cs
+++ Booking/Helpers/ValidationResult.cs
@@ -32,10 +32,23 @@ namespace BookingApp.Helpers
             if (date.Length > 40) return new ValidationResult(false, "Unreasonable date string size");
 
             // lexical content validation
-            var pattern = "^((\\d{4}-\\d{2}-\\d{2})T\\d{2}:\\d{2}:\\d{2}(.\\d{1,5})?(Z|\\+\\d{2}(:\\d{2})?))$";
+            var pattern = "^(?<datetime>(?<date>\\d{4}-\\d{2}-\\d{2})T(?<time>\\d{2}:\\d{2}:\\d{2}(.\\d{1,5})?(?<timezone>Z|\\+\\d{2}(:\\d{2})?)))$";
+            var patternUTC = "23:00:00(.\\d{1,5})?Z";
+            var patternLocale = "00:00:00+00:0(1|2):00";
             Match m = Regex.Match(date, pattern);
 
-            if (!m.Success) return new ValidationResult(false, "Invalid date string, date must follow ISO 8601 format and be UTC");
+            if (!m.Success) return new ValidationResult(false, "Invalid date string, date must follow ISO 8601 and be at midnight Central European Time");
+
+            if (m.Groups["timezone"].Value.StartsWith("Z"))
+            {
+                Match utcMatch = Regex.Match(m.Groups["time"].Value, patternUTC);
+                if (!utcMatch.Success) return new ValidationResult(false, "Invalid UTC time input, must follow YYYY-MM-DDT23:00:00Z");
+            }
+            else
+            {
+                Match localeMatch = Regex.Match(m.Groups["time"].Value, patternLocale);
+                if (!localeMatch.Success) return new ValidationResult(false, "Invalid Local time input, must follow YYYY-MM-DDT00:00:00+01:00 or YYYY-MM-DDT00:00:00+02:00");
+            }
 
             return new ValidationResult(true);
         }
