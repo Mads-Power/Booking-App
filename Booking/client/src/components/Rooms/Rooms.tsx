3a004ef99c14a2893e2115a3c62476a9395b09ea
Martin <martin.johansen99@hotmail.com>
2023-01-12 10:11:18 +0100

    Initial look of "Storerommet" view with desks

--- Booking/client/src/components/Rooms/Rooms.tsx
+++ Booking/client/src/components/Rooms/Rooms.tsx
@@ -28,7 +28,7 @@ export const Rooms = () => {
   const { selectedDate: date, setSelectedDate: setDate }: DateContextType =
     useDateContext(); //'2023-01-02T13:42:16.115Z'
   const [room, setRoom] = useState(1);
-  const [ selectedRoom, setSelectedRoom ] = useState<Room>()
+  const [selectedRoom, setSelectedRoom] = useState<Room>()
   const occupiedSeats = useBookingsByRoom(room, date);
   const loggedInUser = useUserContext().user;
 
@@ -55,22 +55,34 @@ export const Rooms = () => {
     }
   };
 
-  const renderIsOccupied = (seatId: number) => {
-    const predicate =
-      occupiedSeats.data?.find((o) => o.seatId == seatId) != undefined;
-    return predicate;
-  };
+  const chooseDeskFill = (seatId: number) => {
+    let fillHex = "";
+    const occupied = occupiedSeats.data?.find(seat => seat.seatId === seatId);
+    if(occupied) {
+      // If the seat is booked by the current logged in user, set the `fillHex` to be green, if not, set the color to be blue
+      fillHex = occupiedSeats.data?.find(seat => seat.userId === loggedInUser.id) ? "#68B984" : "#3981F1"
+    } else {
+      // If seat is free, set `fillHex` to the color black
+      fillHex = "#000000"
+    }
+    return fillHex
+  }
 
-  const renderOccupiedByLoggedInUser = (seatId: number) => {
-    const predicate =
-      occupiedSeats.data?.find(
-        (o) => o.seatId == seatId && o.userId == loggedInUser.id
-      ) != undefined;
-    return predicate;
-  };
+  const handleDeskClick = (id: number) => {
+  }
+  const renderDeskSvgs = (seatId: number) => {
+    return <svg xmlns="http://www.w3.org/2000/svg"
+     height="48"
+      width="48"
+      onClick={() => handleDeskClick(seatId)}
+      fill = {chooseDeskFill(seatId)}
+      >
+        <path d="M4 36V12h40v24h-3v-5h-9.5v5h-3V15H7v21Zm27.5-16H41v-5h-9.5Zm0 8H41v-5h-9.5Z" />
+      </svg>
+  }
 
   return (
-    <>
+    <div className="h-full overflow-hidden flex flex-col gap-4">
       <Container>
         <WeekViewDatePicker />
       </Container>
@@ -100,12 +112,46 @@ export const Rooms = () => {
       </div>
       </Container>
 
+      {/* 1 = Storerommet / 2 = Lillerommet */}
     {room === 1 ? (
+        <div className="w-80 mx-auto bg-slate-400 bg-opacity-10 flex flex-col p-4 gap-y-8">
+          {selectedRoom ? (
+            <div className="border border-solid border-black p-4 flex flex-col">
+              <div className="flex flex-col">
+                <div className="flex flex-row-reverse gap-x-6">
+                  {renderDeskSvgs(1)}
+                  {renderDeskSvgs(2)}
+                  {renderDeskSvgs(3)}
+                </div>
+                <div className="flex flex-row-reverse gap-x-6">
+                  {renderDeskSvgs(4)}
+                  {renderDeskSvgs(5)}
+                  {renderDeskSvgs(6)}
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div>Kunne ikke hente valgt rom</div>
+          )}
     <div>
-      <h1>{selectedRoom?.name}</h1>
+            {selectedRoom ? (
+              <div className="border border-solid border-black p-4 flex flex-col">
+                <div className="flex flex-col">
+                  <div className="flex flex-row-reverse gap-x-6">
+                    {renderDeskSvgs(7)}
+                    {renderDeskSvgs(8)}
+                  </div>
+                  <div className="flex flex-row-reverse gap-x-6">
+                    {renderDeskSvgs(9)}
+                    {renderDeskSvgs(10)}
+                  </div>
+                </div>  
+              </div>
+            ): (<></>)}
+          </div>
     </div>) : (
     <div>dsa</div>
     )}
-    </>
+    </div>
   );
 };
