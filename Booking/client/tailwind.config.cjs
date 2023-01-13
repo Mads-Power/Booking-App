8fe197b38aa500be1c3036f4e8c818589c7335ee
Martin <martin.johansen99@hotmail.com>
2023-01-13 10:58:41 +0100

    Added tailwind funciton to target all child elements

--- Booking/client/tailwind.config.cjs
+++ Booking/client/tailwind.config.cjs
@@ -7,5 +7,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    }
+  ],
 }
