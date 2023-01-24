2b2647089b8cd0da1cce13481994c7a7c5c38409
Martin <martin.johansen99@hotmail.com>
2023-01-24 15:41:35 +0100

    If an error is recieved from the api, an error will be thrown

--- Booking/client/src/api/useRemoveBookingMutation.ts
+++ Booking/client/src/api/useRemoveBookingMutation.ts
@@ -15,21 +15,18 @@ export const removeBooking = async ({ userId, date }: DeleteBooking) => {
       date,
     }),
   };
-  const res = await fetch(url, requestOptions);
-
-  // return as json if error, so we can log error message
-  if (res.status == 400) {
-    return res.json();
-  } else {
-    return res;
-  }
+  await fetch(url, requestOptions).then(res => {
+    if(res.ok) return res.json();
+    let error = new Error("Http status code: " + res.status);
+    error.cause = res.statusText;
+    throw error;
+  });
 };
 
 export const useRemoveBookingMutation = () => {
   return useMutation({
     mutationFn: removeBooking,
     onError: error => {
-      console.log(error);
     },
     onMutate: m => {
       console.log(m);
