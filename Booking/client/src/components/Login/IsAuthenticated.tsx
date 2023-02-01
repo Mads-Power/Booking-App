869ba6fcf06e254f5c749835644b83be6c70f556
Emil Johannessen <emil.johannessen@itverket.no>
2023-02-01 13:49:36 +0100

    Fixed auth

--- Booking/client/src/components/Login/IsAuthenticated.tsx
+++ Booking/client/src/components/Login/IsAuthenticated.tsx
@@ -3,6 +3,7 @@ import { userAtom } from "@components/Provider/app";
 import { CircularProgress } from "@mui/material";
 import { User } from "@type/user";
 import { useAtom } from "jotai";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const IsAuthenticated = ()=> {
@@ -12,25 +13,27 @@ export const IsAuthenticated = ()=> {
 
   const { data, isSuccess, isFetched, error, isError } = useIsAuthenticated();
 
-  if(isError) {
-    console.log(isFetched)
-    navigate('/login')
-  }
-
-  if (isFetched) {
-    if (isSuccess) {
-      // setUser(data as User);
-      window.sessionStorage.setItem('user', JSON.stringify(data));
-      navigate('/home')
-    } else {
-      console.error('Kunne ikke autentisere brukeren')
+  useEffect(() => {
+    
+    // First, we get the (possibly) authenticated user
+    // If they're not authenticated, redirect them back to the login screen
+    // If they're authenticated, redirect them to the home screen
+    if(isError) {
       navigate('/login')
     }
-  }
 
-  // First, we get the (possibly) authenticated user
-  // If they're not authenticated, redirect them back to the login screen
-  // If they're authenticated, redirect them to the home screen
+    if (isFetched) {
+      if (isSuccess) {
+        setUser(data as User);
+        window.sessionStorage.setItem('user', JSON.stringify(data));
+        navigate('/home')
+      } else {
+        console.error('Kunne ikke autentisere brukeren')
+        navigate('/login')
+      }
+    }
+
+   });
 
   return (
     <div className="flex flex-col">
